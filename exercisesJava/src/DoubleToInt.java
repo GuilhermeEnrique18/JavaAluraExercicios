1b7ecc624b670edf2c49f1f60552fc9d9ab8908c
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-25 15:37:53 -0300

    correcao casting

--- exercisesJava/src/DoubleToInt.java
+++ exercisesJava/src/DoubleToInt.java
@@ -1,8 +1,7 @@
 public class DoubleToInt {
     public static void main(String args[]){
         double n1 = 4.5;
-        int n2 = 4;
-        int n1Int =  (int) n1;
+        int n1Int = (int) n1;
         System.out.println(n1Int);
     }
 }
