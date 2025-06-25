967793cb6d93fb707785995bcee833b70e8a40ff
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-25 16:17:13 -0300

    calculo desconto

---
+++ exercisesJava/src/descontoSobreValorOriginal.java
@@ -0,0 +1,12 @@
+public class descontoSobreValorOriginal {
+    public static void main(String args[]){
+        double precoOriginal = 450.00;
+        double percentualDesconto = 25;
+        double desconto = precoOriginal * (percentualDesconto/100);
+        double precoDescontado = precoOriginal - desconto;
+
+        System.out.printf("O valor original do produto é: %.2f. " +
+                "O desconto solicitado foi de %.2f porcento. Produto com desconto: %.2f",
+                precoOriginal,percentualDesconto, precoDescontado);
+    }
+}
