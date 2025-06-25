7a566831164ffda235a9034950e402fe0b36dbc4
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-25 16:00:53 -0300

    Produto x Quantidade calculando valor total do estoque

---
+++ exercisesJava/src/ProdutoXQuantidade.java
@@ -0,0 +1,10 @@
+public class ProdutoXQuantidade {
+    public static void main(String args[]){
+        double precoProduto = 10.50;
+        int quantidade = 250;
+        double total = precoProduto * quantidade;
+
+        System.out.printf("O valor total do estoque é de: %.2f", total);
+        
+    }
+}
