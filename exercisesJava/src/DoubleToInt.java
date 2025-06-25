474bf1aaa82d127cf04ba1f7e0048f053ef93197
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-25 15:23:58 -0300

    adicao de exercicios media e casting convertendo de double para int

---
+++ exercisesJava/src/DoubleToInt.java
@@ -0,0 +1,8 @@
+public class DoubleToInt {
+    public static void main(String args[]){
+        double n1 = 4.5;
+        int n2 = 4;
+        int n1Int =  (int) n1;
+        System.out.println(n1Int);
+    }
+}
---
+++ exercisesJava/src/Media.java
@@ -0,0 +1,8 @@
+public class Media {
+    public static void main(String args[]){
+        double n1 = 4.5;
+        double n2 = 4.5;
+        double media = (n1 + n2) / 2;
+        System.out.printf("A media é: %.2f", media);
+    }
+}
