b04116691a82bad5abf8cb7f67ef796558765102
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-26 18:05:16 -0300

    Fatorial de um numero

---
+++ exercisesJava/src/Fatorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero para saber seu fatorial: ");
+        int n = sc.nextInt();
+
+        int nFatorial = 1;
+
+        for(int i = 1; i <= n; i++){
+            nFatorial *= i;
+        }
+       System.out.println(nFatorial);
+    }
+}
