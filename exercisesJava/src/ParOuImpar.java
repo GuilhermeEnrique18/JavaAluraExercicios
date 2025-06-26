6a54fe6a15858509bb1187737e24edf2d5d59a73
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-26 17:52:26 -0300

    Par ou Impar

---
+++ exercisesJava/src/ParOuImpar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um numero para saber se par ou impar:");
+        int n = sc.nextInt();
+
+        if (n%2 == 0){
+            System.out.printf("%d é par", n);
+        } else{
+            System.out.printf("%d é ímpar", n);
+        }
+    }
+}
