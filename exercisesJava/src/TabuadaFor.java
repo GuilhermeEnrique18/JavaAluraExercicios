55b6595fede73f78314a87602d48170ead52b832
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-26 17:48:53 -0300

    Tabuada usando for

--- exercisesJava/src/TabuadaFor.java
+++ exercisesJava/src/TabuadaFor.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
 public class TabuadaFor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numTabuada = 0;
+        int numCalculado = 0;
+
+        System.out.println("Digite um número para calcular a tabuada: ");
+        numTabuada = sc.nextInt();
+
+        for (int i = 0; i <= 10; i++) {
+            numCalculado = numTabuada * i;
+            System.out.printf("%d x %d = %d\n",i,numTabuada,numCalculado);
+
+        }
+    }
 }
