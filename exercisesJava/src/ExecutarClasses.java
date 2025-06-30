a752bf7243b444e4eb57c54d4b3133f71f1005fc
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-30 17:40:15 -0300

    Adicao de exercicio03 implementando classe Musica, seus atributos e metodos

--- exercisesJava/src/ExecutarClasses.java
+++ exercisesJava/src/ExecutarClasses.java
@@ -1,8 +1,19 @@
 public class ExecutarClasses {
     public static void main(String[] args) {
+        Musica musica01 = new Musica();
 
-        
+        musica01.titulo = "Me namora";
+        musica01.artista = "Natiruts";
+        musica01.anoLancamento = 2012;
 
+        musica01.exibeFichaTecnica();
+
+        musica01.avalia(4.5);
+        musica01.avalia(3.8);
+        musica01.avalia(5.0);
+
+        double mediaAvaliacoes = musica01.getMedia();
+        System.out.printf("Média das avaliações: %.2f", mediaAvaliacoes);
 
 
         //Pessoa pessoa01 = new Pessoa();
---
+++ exercisesJava/src/Musica.java
@@ -0,0 +1,22 @@
+public class Musica {
+    String titulo;
+    String artista;
+    int anoLancamento;
+    double somaAvaliacao;
+    int numAvaliacoes;
+
+
+
+
+    void exibeFichaTecnica(){
+        System.out.printf("Titulo: %s\nArtista: %s\nAno de lançamento: %d\n",titulo,artista,anoLancamento);
+    }
+    void avalia(double nota) {
+        somaAvaliacao += nota;
+        numAvaliacoes++;
+    }
+    double getMedia(){
+        return somaAvaliacao / numAvaliacoes;
+    }
+
+}
