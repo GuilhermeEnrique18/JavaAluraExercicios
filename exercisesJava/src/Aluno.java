e4a65012d928328cae6204c7f1f6140e260d9fbf
Guilherme <enriqueguilherme1209@gmail.com>
2025-06-30 18:16:56 -0300

    Adicao de exercicio05 adicao da classe Aluno

---
+++ exercisesJava/src/Aluno.java
@@ -0,0 +1,9 @@
+public class Aluno {
+    String nome;
+    int idade;
+
+    void exibeInformacoes() {
+        System.out.printf("Nome do aluno: %s\n",nome);
+        System.out.printf("Idade do aluno: %d ", idade);
+    }
+}
--- exercisesJava/src/ExecutarClasses.java
+++ exercisesJava/src/ExecutarClasses.java
@@ -1,15 +1,10 @@
 public class ExecutarClasses {
     public static void main(String[] args) {
 
-        Carro carro1 = new Carro();
+        Aluno meuAluno = new Aluno();
+        meuAluno.nome = "Guilherme";
+        meuAluno.idade = 21;
 
-        carro1.cor = "vermelho";
-        carro1.ano = 2010;
-        carro1.modelo = "SUV";
-
-        int resp = carro1.calculaIdadeCarro();
-        carro1.exibirFichaTecnica();
-        System.out.printf("\nIdade do carro: %d",resp);
-
+        meuAluno.exibeInformacoes();
     }
 }
